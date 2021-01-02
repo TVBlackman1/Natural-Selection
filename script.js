5010ca90770d3f172cf299fe9660e658d20a3677
tvblackman1 <vbif.misha2012@yandex.ru>
2021-01-02 21:43:51 +0300

    prepare to add graphic

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 import {Canvas} from "./classes/present/Canvas";
-import {setLogicalNamespace} from "./classes/logic/LogicalNamespace";
+import {setLogicalNamespace, getLogicalNamespace} from "./classes/logic/LogicalNamespace";
 import {LogicalProcess} from "./classes/logic/LogicalProcess";
 import {BacteriaGreen} from "./classes/logic/BacteriaGreen";
+import {Bacteria} from "./classes/logic/Bacteria";
 
 window.onload = () => {
     setLogicalNamespace()
@@ -12,4 +13,26 @@ window.onload = () => {
 
     const canvas = new Canvas()
     canvas.start()
+
+    let plot = document.querySelector("#info").plot(data, options).data("plot");
+
+
+    setInterval(() => {
+        const list = getLogicalNamespace().objectLists.BacteriaGreenList.objects
+
+        const speedData = {}
+        for(let i = -1; i < 5; i += 0.2)
+            speedData[i] = (
+                list.reduce((accumulate, bacteria)=> {
+                    if(bacteria instanceof Bacteria)
+                        if (Math.abs(bacteria.speed - i) <= 0.1) {
+                            return accumulate + 1
+                        }
+                    return accumulate
+                }, 0))
+
+        // -1 to 5
+
+        // let graphic = d3.line(Object.keys(speedData), Object.values(speedData))
+    }, 1500)
 }
